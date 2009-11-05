a22c013486b305072d6df859bf91ffd16d9b07fd
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-11-05 12:33:33 +0530

    rename edit to edit profile in users admin page

--- media/script/admin/users.js
+++ media/script/admin/users.js
@@ -1,5 +1,14 @@
 // common.js should be included before this file.
 
+function defaultActions(type, item){
+    return '<a class="star ' + ((item.is_starred)? 'on': '') + '" href="/api/' + type + '/' + item.key + '/toggle_star/"></a>\
+    <a class="awesome-button cool" rel="approve" href="/api/' + type + '/' + item.key + '/approve/"><span class="symbol">&#10004;</span> Approve</a>\
+    <a class="awesome-button warm" rel="unapprove" href="/api/' + type + '/' + item.key + '/unapprove/"><span class="symbol">&#10004;</span> Unapprove</a>\
+    <a class="awesome-button" rel="edit" href="/api/' + type + '/' + item.key + '/edit/"><span class="symbol">&minus;</span> Edit Profile</a>\
+    <a class="awesome-button" rel="delete" href="/api/' + type + '/' + item.key + '/delete/"><span class="symbol">&times;</span> Delete</a>\
+    <a class="awesome-button" rel="undelete" href="/api/' + type + '/' + item.key + '/undelete/"><span class="symbol">&times;</span> Undelete</a>';
+}
+
 function createItemHTML(user){
     return dataListEntry(user, user.nickname, defaultActions('users', user), defaultTags());
 }
