c5804a7cebab3b51783422fe01678ea0bc528da5
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-11-09 10:42:52 +0530

    remove unnecessary code from main.js

--- media/script/main.js
+++
@@ -1,7 +0,0 @@
-(function($){
-    $(function(){
-       //$('.roundbox').corner('16px tl tr bl br ').css('opacity', 0.5); 
-        //$('.roundbox').liquidCanvas("[fill{color:#ffffff;}] => roundedRect{radius:15}")
-    });
-   
-})(jQuery);
