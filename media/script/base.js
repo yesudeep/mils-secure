c055548e063f56f93ade31c26bbcd047445ccc91
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-11-09 11:46:35 +0530

    show navlink captions using tips

---
+++ media/script/base.js
@@ -0,0 +1,29 @@
+jQuery(function(){
+    var elements = {  
+        navlinks: jQuery('ul.navlinks > li > a[href][title]')
+    };
+    
+    elements.navlinks.qtip({
+        position: {
+            corner: {
+                target: 'bottomMiddle',
+                tooltip: 'center'
+            }
+        },
+        content: {
+            text: false
+        },
+        style: {
+            fontFamily: '"Lucida Grande", "Corbel", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", "Verdana", "Verdana Ref", "sans-serif"',
+            border: {
+                radius: 5,
+                width: 0
+            },
+            name: 'dark',
+            tip: 'topMiddle'
+        },
+        show: {
+            delay: 5
+        }
+    });
+});
