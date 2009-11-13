0460dc0b2e8af19f916a4026a7088342ddd1fc22
Yesudeep Mangalapilly <yesudeep@gmail.com>
2009-11-13 20:32:59 +0530

    about page and separate jquery tools

--- media/script/about.js
+++ media/script/about.js
@@ -1,11 +1,14 @@
 jQuery(function(){
-    jQuery("#about_alumni").innerfade({
+    /*jQuery("#about_alumni").innerfade({
       animationtype: 'fade',
       speed: 'slow',
       timeout: 12000,
       type: 'sequence',
       containerheight: '250px'
-    });
+    });*/
+    var elements = {
+        
+    };
 });
 
     
