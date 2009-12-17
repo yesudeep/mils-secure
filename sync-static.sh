d056d89a97785db8bc906b10461bc46bd9e3b368
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-12-17 10:26:35 +0530

    update the location of the static file directory in the sync-static.sh script

--- sync-static.sh
+++ sync-static.sh
@@ -3,5 +3,5 @@
 # Ideally this should be a part of puppy configurable via puppy.yaml.
 
 cp -R app/public/ $HOME/DropBoxes/mils-alumni/Dropbox/Public/public/
-rsync -avc app/public/ yesudeep@assets.milsalumni.org:/var/www/assets.milsalumni.org/
+rsync -avc app/public/ yesudeep@assets.milsalumni.org:/var/www/assets/milsalumni.org/public/
 
