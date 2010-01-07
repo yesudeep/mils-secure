25c6f76841105c74eb2f3ef97840340151b3f979
Yesudeep Mangalapilly <yesudeep@mia.local>
2010-01-07 18:51:09 +0530

    check fee calculation with real world data

--- app/fee_calculation.py
+++ app/fee_calculation.py
@@ -20,22 +20,60 @@ if __name__ == "__main__":
             self.fee = fee
             self.for_participant_count = for_participant_count
     fees = [
-        Fee(3000, 1),
-        Fee(2000, 5),
-        Fee(1000, 10),
+        Fee(6000, 15),
+        Fee(5000, 30),
+        Fee(3000, 40),
     ]
-    assert calculate_fee(fees, 0) == 3000
-    assert calculate_fee(fees, 1) == 3000
-    assert calculate_fee(fees, 2) == 3000
-    assert calculate_fee(fees, 3) == 3000
-    assert calculate_fee(fees, 4) == 3000
-    assert calculate_fee(fees, 5) == 2000
-    assert calculate_fee(fees, 6) == 2000
-    assert calculate_fee(fees, 7) == 2000
-    assert calculate_fee(fees, 8) == 2000
-    assert calculate_fee(fees, 9) == 2000
-    assert calculate_fee(fees, 10) == 1000
-    assert calculate_fee(fees, 11) == 1000
-    assert calculate_fee(fees, 12) == 1000
+    assert calculate_fee(fees, 0) == 6000
+    assert calculate_fee(fees, 1) == 6000
+    assert calculate_fee(fees, 2) == 6000
+    assert calculate_fee(fees, 3) == 6000
+    assert calculate_fee(fees, 4) == 6000
+    assert calculate_fee(fees, 5) == 6000
+    assert calculate_fee(fees, 6) == 6000
+    assert calculate_fee(fees, 7) == 6000
+    assert calculate_fee(fees, 8) == 6000
+    assert calculate_fee(fees, 9) == 6000
+    assert calculate_fee(fees, 10) == 6000
+    assert calculate_fee(fees, 11) == 6000
+    assert calculate_fee(fees, 12) == 6000
+    assert calculate_fee(fees, 13) == 6000
+    assert calculate_fee(fees, 14) == 6000
+    assert calculate_fee(fees, 15) == 6000
+    assert calculate_fee(fees, 16) == 6000
+    assert calculate_fee(fees, 17) == 6000
+    assert calculate_fee(fees, 18) == 6000
+    assert calculate_fee(fees, 19) == 6000
+    assert calculate_fee(fees, 20) == 6000
+    assert calculate_fee(fees, 21) == 6000
+    assert calculate_fee(fees, 22) == 6000
+    assert calculate_fee(fees, 23) == 6000
+    assert calculate_fee(fees, 24) == 6000
+    assert calculate_fee(fees, 25) == 6000
+    assert calculate_fee(fees, 26) == 6000
+    assert calculate_fee(fees, 27) == 6000
+    assert calculate_fee(fees, 28) == 6000
+    assert calculate_fee(fees, 29) == 6000
+    assert calculate_fee(fees, 30) == 5000
+    assert calculate_fee(fees, 31) == 5000
+    assert calculate_fee(fees, 32) == 5000
+    assert calculate_fee(fees, 33) == 5000
+    assert calculate_fee(fees, 34) == 5000
+    assert calculate_fee(fees, 35) == 5000
+    assert calculate_fee(fees, 36) == 5000
+    assert calculate_fee(fees, 37) == 5000
+    assert calculate_fee(fees, 38) == 5000
+    assert calculate_fee(fees, 39) == 5000
+    assert calculate_fee(fees, 40) == 3000
+    assert calculate_fee(fees, 41) == 3000
+    assert calculate_fee(fees, 42) == 3000
+    assert calculate_fee(fees, 43) == 3000
+    assert calculate_fee(fees, 44) == 3000
+    assert calculate_fee(fees, 45) == 3000
+    assert calculate_fee(fees, 46) == 3000
+    assert calculate_fee(fees, 47) == 3000
+    assert calculate_fee(fees, 48) == 3000
+    assert calculate_fee(fees, 49) == 3000
+    assert calculate_fee(fees, 50) == 3000
     print "all tests passed"
 
